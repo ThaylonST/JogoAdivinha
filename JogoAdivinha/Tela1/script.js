8e75336b0b402a09cfa7aa236c0483c2a1e8de26
ThaylonST <136936975+ThaylonST@users.noreply.github.com>
2024-06-11 14:51:12 -0300

    Add files via upload

---
+++ JogoAdivinha/Tela1/script.js
@@ -0,0 +1,12 @@
+var botao = document.querySelector("#botao").addEventListener("click", function(event){
+
+event.preventDefault();
+
+var nome = document.querySelector("#nome").value;
+
+window.location.href = "http://127.0.0.1:5500/Tela2/TelaDoJogo.html";
+
+
+})
+
+
